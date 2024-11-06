22336da6fbf80e2c1961c2c84acbf0367882a3fc
shapled <shapled@shapled.com>
2024-11-06 23:33:12 +0800

    Update cask.bat

--- bin/cask.bat
+++ bin/cask.bat
@@ -1,5 +1,33 @@
 @echo off
-bash %~dp0cask %*
 
-REM %~dp0 expands to match CASK_BIN_DIRECTORY.
-REM %* passes all arguments to "bash cask"
+setlocal enabledelayedexpansion
+
+set "ARGS=%*"
+set "EMACS=%CASK_EMACS%"
+for /f "delims=" %%i in ('where cask.bat') do (set CASK=%%i & goto :doneCask)
+:doneCask
+if "%EMACS%"=="" set "EMACS=%EMACS%"
+if "%EMACS%"=="" set "EMACS=emacs"
+
+if "%~1"=="" (
+    set "subcommand=install"
+) else (
+    set "subcommand=%1"
+    shift /1
+    for /f "tokens=1,* delims= " %%a in ("%*") do set ARGS=%%b
+)
+
+for %%I in ("%CASK%") do set "SRCDIR_=%%~dpI"
+for %%I in ("%SRCDIR_%") do set "SRCDIR=%%~dpI.."
+
+if /i "%subcommand%"=="emacs" (
+    "%EMACS%" -Q -L "%SRCDIR%" -l "%SRCDIR%\cask" --eval "(cask--initialize (expand-file-name default-directory))" %ARGS%
+) else if /i "%subcommand%"=="exec" (
+    set "EMACSLOADPATH=%CASK% load-path"
+    set "PATH=%CASK% path;%PATH%"
+    "%EMACS%" %ARGS%
+) else (
+    "%EMACS%" -Q --script "%SRCDIR%\cask-cli.el" -- %subcommand% %ARGS%
+)
+
+endlocal
